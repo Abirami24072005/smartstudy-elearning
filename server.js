3a2ab417b00c3d47171b30e0681e91efc7d4e2dc
Abirami24072005 <alaguprasanna1812@gmail.com>
2026-04-15 14:23:46 +0530

    fixed port for deployment

--- server.js
+++ server.js
@@ -19,6 +19,8 @@ app.post('/pay', (req, res) => {
   }, 1500)
 })
 
-app.listen(3000, () => {
-  console.log("Backend running on http://localhost:3000")
-})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Backend running on port ${PORT}`);
+});
